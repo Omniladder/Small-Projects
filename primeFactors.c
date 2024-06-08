132a866d2046df79b29a2c319564e98ee2f19142
Omniladder <dustintobrien@gmail.com>
2024-06-08 16:41:07 -0400

    Prime Factorization Calculator

---
+++ primeFactors.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+	int number;
+	printf("What do you want to get the prime factors of???\n");
+	scanf("%d", &number);
+	
+	printf("\n\nThe Prime Numbers Are:\n");
+	for(int i = 2; i <= sqrt(number); i++)
+	{
+		while(number % i == 0)
+		{
+			printf("%d\n", i);
+			number /= i;
+		}
+	}
+	if(number != 1)
+	printf("%d\n", number);
+
+}
