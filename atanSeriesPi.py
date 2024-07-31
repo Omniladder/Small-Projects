106b2926d7c6271d8f65d4f4d04602ee92d553e3
Omniladder <dustintobrien@gmail.com>
2024-07-30 22:32:46 -0400

    Added Margin of error and renamed pi calculators to make easier to find

--- atanSeriesPi.py
+++ atanSeriesPi.py
@@ -6,8 +6,13 @@ PiSum = 0
 neg = 1
 
 for i, j in enumerate([j * 2 + 1 for j in range(iterations)]):
-    PiSum += 1 / j * neg
+    error = 1 / j * neg # Since it converge the sum of all terms after should be smaller than new term if i'm correct might be wrong
+    PiSum += error
     neg *= -1
 
 PiSum *= 4
+error = abs(4 * error)
+
+print()
 print(f"PI Roughly Equals {PiSum}")
+print(f"Margin of Error is +- {error}")
