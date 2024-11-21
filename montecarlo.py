23afb1e5b36d7d3db70b40e103afb97e2eed6ef0
Omniladder <dustintobrien@gmail.com>
2024-11-21 10:34:15 -0500

    Simple Monte Carlo Fairly Slow Because O(n^2)

--- montecarlo.py
+++ montecarlo.py
@@ -3,9 +3,8 @@ n = 5000 # Higher this variable more accurate the approximation is
 count = 0
 
 for x in range(n):
-    print(x)
     for y in range(n):
         if ((x / n)**2 + (y / n)**2)**(1/2) <= 1:
             count += 1
 
-print("Approximation of PI", count / 2)
+print("Approximation of PI", 4* count /  n ** 2)
